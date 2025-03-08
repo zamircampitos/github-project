a1d55341f249cf88363d85d1835de2a9ece685c3
zamircampitos <zamircampitos@gmail.com>
2025-03-08 05:31:50 +0000

    Add 3.py

---
+++ src/3.py
@@ -0,0 +1,6 @@
+import random
+
+def get_random_code(length=8):
+    """Generate a random code of the specified length."""
+    characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    return ''.join(random.choice(characters) for _ in range(length))
